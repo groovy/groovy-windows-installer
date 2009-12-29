b9ebc2cf7a7bc15852609c5e22a9114e2f6bc88a
jbaumann <jbaumann@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-12-29 16:56:49 +0000

    Added a few tests to the testcase

--- trunk/groovy/modules/installers/windows/NSIS-Groovy/TestInstallation.groovy
+++ trunk/groovy/modules/installers/windows/NSIS-Groovy/TestInstallation.groovy
@@ -5,7 +5,9 @@
  */
 
 import groovy.swing.SwingXBuilder
-import org.codehaus.groovy.scriptom.*;
+import org.codehaus.groovy.scriptom.*
+import groovy.util.Eval
+import groovy.lang.GroovyShell
 
 import griffon.builder.jide.JideBuilder
 // 1.5 import org.kordamp.groovy.swing.jide.JideBuilder
@@ -88,5 +90,45 @@ public class TestInstallation extends GroovyTestCase {
           def result = scriptControl.Eval('2.0 + 2.0;')
           assertEquals 4, result
         }
+    }
+    
+    void testGaelykIsInstalled() {
+        def groovyVar = System.getenv("GROOVY_HOME");
+        assertNotNull groovyVar
+        File gd = "$groovyVar/Supplementary/Gaelyk" as File
+        assertEquals true, gd.exists()
+        List files = gd.list()
+        assertTrue files.size != 0
+        assertTrue files.find{it =~ /gaelyk/} != null
+    }
+
+    void testGPars() {
+    	String gparsScript = """
+			import static groovyx.gpars.actor.Actors.actor
+			def decryptor = actor { 
+				loop {
+					react {message -> 
+						if (message instanceof String) reply message.reverse() 
+						else stop() 
+					}
+				}
+			}
+			def console = actor { 
+				decryptor.send 'lellarap si yvoorG' 
+				react { 
+					value = 'Decrypted message: ' + it 
+					decryptor.send false 
+				} 
+			}
+			[decryptor, console]*.join()
+			return value
+    	"""
+    	
+		Object result = Eval.me(gparsScript)
+		assertEquals result, "Decrypted message: Groovy is parallel"
+    }
+    
+    void testEasyb() {
+    	println Eval.me("org.easyb.BehaviorRunner.main('easybTest.story')")
     }
 }
