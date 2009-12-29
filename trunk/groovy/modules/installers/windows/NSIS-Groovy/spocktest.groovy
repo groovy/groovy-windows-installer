ecc112756405725a428faa607d6ce49769a8db09
jbaumann <jbaumann@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-12-29 14:04:44 +0000

    Added easyb and a test for spock, changed order of modules to be alphabetic

---
+++ trunk/groovy/modules/installers/windows/NSIS-Groovy/spocktest.groovy
@@ -0,0 +1,13 @@
+import spock.lang.Specification
+
+class HelloSpock extends Specification {
+    def "can you figure out what I'm up to?"() {
+        expect:
+        name.size() == size
+
+        where:
+        name << ["Kirk", "Spock", "Scotty"]
+        size << [4, 5, 6]
+    }
+}
+
